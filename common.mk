d0688ba97df8f9fbae546aa77328f6b3048a33c0
Alexander Winkowski <dereference23@outlook.com>
2021-07-20 14:08:14 +0200

    sm7125-common: Import powerhint.json
    * From sunfish-user 11 RQ3A.210705.001 7380771 release-keys
    
    Change-Id: I09399706d007a0e3de89945bd9fe269bd7fa78e3

--- common.mk
+++ common.mk
@@ -260,6 +260,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json    
+
 # QTI FWK Detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
